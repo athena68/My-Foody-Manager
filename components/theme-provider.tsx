24fb265fef7493512db7d4fe42e38f8d997c65cd
Tuan <tigonfairy@gmail.com>
2025-02-26 21:11:56 +0700

    Fix next build for Vercel deployment

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,8 +1,9 @@
 "use client"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import type { ThemeProviderProps } from "next-themes"
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+import type { ThemeProviderProps as NextThemesProviderProps } from "next-themes/dist/types"
+
+export function ThemeProvider({ children, ...props }: NextThemesProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
 
